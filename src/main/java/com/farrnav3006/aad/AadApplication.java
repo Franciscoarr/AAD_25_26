86846abd776dbe66c2dc9647f1ffbf18f8bd12a1
Francisco Arrieta <farrnav3006@g.educaand.es>
2025-09-22 12:12:22 +0200

    Proyecto ADD con configuración inicial y banner personalizado

---
+++ src/main/java/com/farrnav3006/aad/AadApplication.java
@@ -0,0 +1,18 @@
+package com.farrnav3006.aad;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AadApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AadApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Ejecutando lógica dentro del método run() del microservicio ADD");
+	}
+}
---
+++ src/test/java/com/farrnav3006/aad/AadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.farrnav3006.aad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
