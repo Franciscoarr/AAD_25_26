86785916c6d7e44f6d4dcf20e970cd2159c6158c
Francisco Arrieta <farrnav3006@g.educaand.es>
2025-10-20 12:26:26 +0200

    Agregado dependencias

--- src/main/java/com/farrnav3006/aad/Act_1_3.java
+++ src/main/java/com/farrnav3006/aad/Act_1_3.java
@@ -1,5 +1,21 @@
 package com.farrnav3006.aad;
 
 
-public class Act_1_3 {
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@Slf4j
+
+public class Act_1_3 implements CommandLineRunner {
+    public static void main(String[] args) {
+        SpringApplication.run(Act_1_3.class, args);
+    }
+
+    @Override
+    public void run(String... args) {
+
+    }
 }
