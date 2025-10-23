1d3e1ce4f6fb9c1a43c33e1ea3c3688715c6f25b
Francisco Arrieta <farrnav3006@g.educaand.es>
2025-10-23 13:53:31 +0200

    Agregar @

--- src/main/java/com/farrnav3006/aad/Alumnos.java
+++ src/main/java/com/farrnav3006/aad/Alumnos.java
@@ -1,48 +1,54 @@
 package com.farrnav3006.aad;
 
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @JacksonXmlRootElement(localName = "students")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+
 public class Alumnos {
 
     private int id;
     private String name;
     private double grade;
 
-    // No-args constructor
-    public Alumnos() {
-    }
-
-    // Constructor with parameters
-    public Alumnos(int id, String name, double grade) {
-        this.id = id;
-        this.name = name;
-        this.grade = grade;
-    }
-
-    // Getters and setters
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public double getGrade() {
-        return grade;
-    }
-
-    public void setGrade(double grade) {
-        this.grade = grade;
-    }
+//    public Alumnos() {
+//    }
+//
+//    public Alumnos(int id, String name, double grade) {
+//        this.id = id;
+//        this.name = name;
+//        this.grade = grade;
+//    }
+//
+//    public int getId() {
+//        return id;
+//    }
+//
+//    public void setId(int id) {
+//        this.id = id;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public double getGrade() {
+//        return grade;
+//    }
+//
+//    public void setGrade(double grade) {
+//        this.grade = grade;
+//    }
 }
 
