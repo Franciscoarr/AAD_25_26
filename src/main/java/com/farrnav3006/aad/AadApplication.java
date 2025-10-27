e86a0aeecdc7cf9f8606ab674a7409bad8c9421b
PepsiPaco <zekronpaco@gmail.com>
2025-10-27 17:17:45 +0100

    Prueba

--- src/main/java/com/farrnav3006/aad/AadApplication.java
+++ src/main/java/com/farrnav3006/aad/AadApplication.java
@@ -4,7 +4,15 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -15,6 +23,10 @@ public class AadApplication implements CommandLineRunner {
 	public static void main(String[] args) {
 		SpringApplication.run(AadApplication.class, args);
 	}
+
+    private static String log_filename = "app.log";
+    private static Charset charset = StandardCharsets.UTF_8; // Codificación por defecto
+
 	@Override
 	public void run(String... args) throws Exception {
 
@@ -29,31 +41,30 @@ public class AadApplication implements CommandLineRunner {
                 log.info("Select an option: ");
 
                 option = scanInt(scanner);
+                scanner.nextLine();
 
                 switch (option) {
                     case 1:
-                        AddEvents();
+                        AddEvents(scanner);
                         break;
                     case 2:
 
                         break;
                     case 3:
+                        ChangeEncoding(scanner);
                         break;
                     case 4:
 
                     default:
                         log.warn("Invalid option");
                 }
-            } while (option != 3);
-
-        } catch (FileNotFoundException e) {
-            log.error("CSV file not found: " + e.getMessage());
+            } while (option != 4);
         } catch (Exception e) {
             log.error("Unexpected error: " + e.getMessage(), e);
         }
 	}
 
-    private int scanInt(Scanner scanner) {
+    private static int scanInt(Scanner scanner) {
         while (true) {
             try {
                 return scanner.nextInt();
@@ -67,8 +78,54 @@ public class AadApplication implements CommandLineRunner {
     private static void AddEvents(Scanner scanner) {
         log.info("Introduce el mensaje del evento: ");
         String mensaje = scanner.nextLine();
+        String fecha = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss").format(new Date());
+
+        String linea = "[" + fecha + "]" + mensaje;
+
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(log_filename, charset))) {
+            bw.write(linea);
+            bw.newLine();
+            log.info("Event added");
+        } catch (IOException e) {
+            log.error("Error al escribir en el archivo: " + e.getMessage());
+        }
+    }
+
+    private static void FilterEvents(Scanner scanner) {
+        log.info("Introduce la fecha del evento (YYYY-MM-DD): ");
+        String fecha = scanner.nextLine();
 
 
+    }
+
+    private static void ChangeEncoding(Scanner scanner) {
+        int option2;
+        do {
+            log.info("===== Change encoding =====");
+            log.info("Current encoding: " + charset.displayName());
+            log.info("1. UTF-8");
+            log.info("2. ISO-8859-1");
+            log.info("3. Exit to main menu");
+            log.info("Select an option: ");
+
+            option2 = scanInt(scanner);
+
+            switch (option2) {
+                case 1:
+                    charset = StandardCharsets.UTF_8;
+                    log.info("Encoding changed to UTF-8");
+                    break;
+                case 2:
+                    charset = StandardCharsets.ISO_8859_1;
+                    log.info("Encoding changed to ISO-8859-1");
+                    break;
+                case 3:
+                    log.info("Returning to previous menu...");
+                    break;
+                default:
+                    log.warn("Invalid option, ");
+            }
+        }while (option2 != 3);
     }
 
 
